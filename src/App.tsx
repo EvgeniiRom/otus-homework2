977a284a8a358b54f64b7e24d9076b9452735a34
EvgeniiRom <evgenijjrmanv118@rambler.ru>
2023-04-01 10:30:13 +0500

    add redirect from bad path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 import GlobalStyle from './components/styled/GlobalStyle';
 import Game from "./pages/Game"
 import Login from './pages/Login';
@@ -11,6 +11,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/game" element={<Game />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </BrowserRouter>
     </>
